515a90979f9a101de775be6ff49f73aca921bf4f
Jordan <jordan.cf13@gmail.com>
2025-01-06 18:31:33 -0500

    Adding search bar and starting styling with Bootstrap

--- src/Business.js
+++ src/Business.js
@@ -1,29 +1,57 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Card from 'react-bootstrap/Card';
 
-function Business() {
-  const business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-  }
+function Business(props) {
+  console.log(props)
+  const {business} = props;
+  // const business = {
+  //   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  //   name: 'MarginOtto Pizzeria',
+  //   address: '1010 Paddington Way',
+  //   city: 'Flavortown',
+  //   state: 'NY',
+  //   zipCode: '10101',
+  //   category: 'Italian',
+  //   rating: 4.5,
+  //   reviewCount: 90
+  // }
 
   return (
+    // <div>
+    //   <img src={business.imageSrc}/>
+    //   <p>{business.name}</p>
+    //   <p>{business.address}</p>
+    //   <p>{business.city}</p>
+    //   <p>{business.state}</p>
+    //   <p>{business.zipCode}</p>
+    //   <p>{business.category}</p>
+    //   <p>{business.rating}</p>
+    //   <p>{business.reviewCount}</p>
+    // </div>
     <div>
-      <img src={business.imageSrc}/>
-      <p>{business.name}</p>
-      <p>{business.address}</p>
-      <p>{business.city}</p>
-      <p>{business.state}</p>
-      <p>{business.zipCode}</p>
-      <p>{business.category}</p>
-      <p>{business.rating}</p>
-      <p>{business.reviewCount}</p>
+      <Card className='m-4'>
+        <Card.Img variant='top' src={business.imageSrc}/>
+        <Card.Body>
+          <Card.Title>
+            {business.name}
+          </Card.Title>
+          <Container>
+            <Row>
+              <Col className='text-start text-nowrap'>{business.address}</Col>
+              <Col className='text-end'>{business.category}</Col>
+            </Row>
+            <Row>
+              <Col className='text-start'>{business.city}</Col>
+              <Col className='text-end'>{business.rating}</Col>
+            </Row>
+            <Row>
+              <Col className='text-start'>{business.zipCode}</Col>
+              <Col className='text-end'>{business.reviewCount}</Col>
+            </Row>
+          </Container>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
--- src/BusinessList.js
+++ src/BusinessList.js
@@ -1,13 +1,65 @@
 import React from "react";
 import Business from "./Business";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function BusinessList() {
-  const businesses = ["business1", "business2", "business3", "business4"];
+  const businesses = [{
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    }, {
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    }, {
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    }, {
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    }];
 
   return (
-    businesses.map((business, index) => {
-      return <Business />;
-    })
+    <Container>
+      <Row>
+        {businesses.map((business, index) => {
+          return (
+          <Col xs={12} md={4} >
+            <Business business={business}/>
+          </Col>
+        );
+        })}
+      </Row>
+    </Container>
   );
 }
 
